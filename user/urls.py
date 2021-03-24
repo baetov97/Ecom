2003d4fa7dd41f43bd701d157112be3c40633161
baetov97 <nurgazy.baetov@gmail.com>
2021-03-24 23:15:37 +0600

    registration with email

--- user/urls.py
+++ user/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth.views import (PasswordChangeView, PasswordResetDoneView
 app_name = 'user'
 urlpatterns = [
     path('signup/', SignUp.as_view(), name='signup'),
+    path('activation/sent/', activation_sent, name='activation_sent'),
     path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
     path('update/', UserUpdateView.as_view(), name='user_update'),
     path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 import jwt
 from django.conf import settings
+from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.sites.shortcuts import get_current_site
 from django.http import HttpResponse
@@ -23,10 +24,10 @@ class SignUp(TemplateView):
     def post(self, request, *args, **kwargs):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            print(1222222222222222222)
             user_data = form.save()
-            print(user_data)
             user = User.objects.get(username=user_data)
+            user.is_active = False
+            user.save()
             token = RefreshToken.for_user(user).access_token
             current_site = get_current_site(request).domain
             relativeLink = reverse('user:email-verify')
@@ -34,7 +35,7 @@ class SignUp(TemplateView):
             email_body = 'Hi ' + user.username + ' Use link bellow to verify your email \n  ' + absurl
             data = {'email_body': email_body, 'to_email': user.email, 'email_subject': 'Verify your email'}
             Util.send_email(data)
-            return redirect('user:login')
+            return redirect('user:activation_sent')
         context = {'form': form}
         return render(request, self.template_name, context)
 
@@ -49,11 +50,17 @@ class VerifyEmail(TemplateView):
             if not user.is_active:
                 user.is_active = True
                 user.save()
-                return render(request, 'user/activation_sent.html')
+                login(request, user)
+                return render(request, 'user/completed-success.html')
         except jwt.ExpiredSignatureError:
-            return render(request, 'user/activation_sent.html')
+            return render(request, 'user/account_invalid.html')
         except jwt.exceptions.DecodeError:
             return render(request, 'user/account_invalid.html')
+        return render(request, 'user/account_invalid.html')
+
+
+def activation_sent(request):
+    return render(request, 'user/activation_sent.html')
 
 
 class UserUpdateView(LoginRequiredMixin, TemplateView):
